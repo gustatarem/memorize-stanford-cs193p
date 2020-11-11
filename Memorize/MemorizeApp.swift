a9586358c42958f8a80d01d6acbac91e7fe99671
Gustavo Tatarem <gus.tatarem@gmail.com>
2020-11-11 15:01:17 -0300

    start project

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Gustavo Tatarem on 10/11/20.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
